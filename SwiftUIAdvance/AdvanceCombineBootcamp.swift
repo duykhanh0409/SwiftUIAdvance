bed54006b7eada49e38d56f9e011f8a073f6e81e
KhanhNguyen <duykhanh.nguyen@unifiedpost.com>
2025-07-07 22:28:01 +0700

    chore: add new file

---
+++ SwiftUIAdvance/AdvanceCombineBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  AdvanceCombineBootcamp.swift
+//  SwiftUIAdvance
+//
+//  Created by Khanh Nguyen on 7/7/25.
+//
+
+import SwiftUI
+
+struct AdvanceCombineBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdvanceCombineBootcamp()
+}
