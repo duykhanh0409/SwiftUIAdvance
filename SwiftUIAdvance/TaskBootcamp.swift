8fd0b4f4571da40017903a0bdc8f5a7a03258bb1
KhanhNguyen49 <duykhanhlb4999@gmail.com>
2025-09-21 15:49:40 +0700

    chore: undertand how to using task and cancel the task

--- SwiftUIAdvance/TaskBootcamp.swift
+++ SwiftUIAdvance/TaskBootcamp.swift
@@ -12,10 +12,13 @@ class TaskBootcampViewModel {
     var image: UIImage? = nil
     
     func fetchImage() async {
+        try? await Task.sleep(nanoseconds: 5_000_000_000)
         do {
             guard let url = URL(string: "https://picsum.photos/200") else { return }
             let (data, _) = try await URLSession.shared.data(from: url)
-            self.image = UIImage(data: data)
+            await MainActor.run {
+                self.image = UIImage(data: data)
+            }
             
         } catch {
             print(error.localizedDescription)
@@ -23,8 +26,21 @@ class TaskBootcampViewModel {
     }
 }
 
+struct TaskBootcampHomeView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                NavigationLink("Click me") {
+                    TaskBootcamp()
+                }
+            }
+        }
+    }
+}
+
 struct TaskBootcamp: View {
     @State private var viewModel = TaskBootcampViewModel()
+//    @State private var fetchImageTask: Task<(), Never>? = nil
     var body: some View {
         VStack(spacing: 40) {
             if let image = viewModel.image {
@@ -34,14 +50,26 @@ struct TaskBootcamp: View {
                     .frame(width: 200, height: 200)
             }
         }
-        .onAppear {
-            Task {
-                await viewModel.fetchImage()
-            }
+        //Hiện tại SwiftUI support cách mới hay hơn là dùng task
+//        action: A closure that SwiftUI calls as an asynchronous task
+        ///     before the view appears. SwiftUI can automatically cancel the task
+        ///     after the view disappears before the action completes. If the
+        ///     `id` value changes, SwiftUI cancels and restarts the task.
+        .task {
+            await viewModel.fetchImage()
         }
+        // Đây là manual way để handle fetch và cancel task
+//        .onDisappear {
+//            fetchImageTask?.cancel()
+//        }
+//        .onAppear {
+//            fetchImageTask = Task {
+//                await viewModel.fetchImage()
+//            }
+//        }
     }
 }
 
 #Preview {
-    TaskBootcamp()
+    TaskBootcampHomeView()
 }
